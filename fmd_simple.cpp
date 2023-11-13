68e6e738a7a614a415f06c9cdb4c5b1655c3bd76
Luca Denti <ldenti@proton.me>
2023-11-13 12:28:44 +0100

    Add very basic FMD implementation (starting from rlcsa official one)

---
+++ fmd_simple.cpp
@@ -0,0 +1,228 @@
+#include "fmd_simple.hpp"
+
+const std::string BASES = "NACGT";
+
+FMDPosition::FMDPosition(CSA::usint forward_start, CSA::usint reverse_start,
+                         CSA::usint end_offset)
+    : forward_start(forward_start), reverse_start(reverse_start),
+      end_offset(end_offset) {}
+
+FMDPosition::FMDPosition()
+    : forward_start(0), reverse_start(0), end_offset(-1) {}
+
+FMDPosition FMDPosition::flip() const {
+  // Swap the two intervals of the bi-interval
+  return FMDPosition(reverse_start, forward_start, end_offset);
+}
+
+bool FMDPosition::operator==(const FMDPosition &other) const {
+  // Compare all the fields.
+  return forward_start == other.forward_start &&
+         reverse_start == other.reverse_start && end_offset == other.end_offset;
+}
+
+std::ostream &operator<<(std::ostream &o, FMDPosition const &position) {
+  // Report both the ranges that we represent.
+  return o << position.forward_start << "-"
+           << (position.forward_start + position.end_offset) << "|"
+           << position.reverse_start << "-"
+           << (position.reverse_start + position.end_offset);
+}
+
+FMD::FMD(const std::string &base_name, bool print) : RLCSA(base_name, print) {}
+
+FMDPosition FMD::extend(FMDPosition range, CSA::usint c, bool backward) const {
+
+  // More or less directly implemented off of algorithms 2 and 3 in "Exploring
+  // single-sample SNP and INDEL calling with whole-genome de novo assembly"
+  // (Li, 2012). However, our character indices are one less, since we don't
+  // allow search patterns to include the end-of-text symbol. We also use
+  // alphabetical ordering instead of the paper's N-last ordering in the FM-
+  // index, and consequently need to assign reverse ranges in alphabetical order
+  // by reverse complement.
+
+  if (backward) {
+
+    // Only allow characters in the index
+    if (c >= CSA::CHARS || this->array[c] == 0) {
+      return EMPTY_FMD_POSITION;
+    }
+    // Only allow DNA bases
+    if (!isBase(c)) {
+      return EMPTY_FMD_POSITION;
+    }
+
+    // We have an array of FMDPositions, one per base, that we will fill in by a
+    // tiny dynamic programming.
+    FMDPosition answers[NUM_BASES];
+
+    for (CSA::usint base = 0; base < NUM_BASES; base++) {
+      // Go through the bases in arbitrary order.
+
+      // Count up the number of characters < this base, including sequence stop
+      // characters.
+      CSA::usint start =
+          this->alphabet->cumulative(base) + this->number_of_sequences - 1;
+
+      // Get a pointer to the bit vector for this letter, which might be NULL if
+      // this base never appeared.
+      CSA::PsiVector *vector = this->array[base];
+
+      if (vector == NULL) {
+
+        // Fill in forward_start and length with the knowledge that this
+        // character doesn't exist. forward_start should never get used, but
+        // end_offset will get used and probably needs to be -1 for empty.
+        answers[base].end_offset = -1;
+
+      } else {
+        // Get an iterator for the bit vector for this character, for
+        // calculating ranks/occurrences.
+        CSA::PsiVector::Iterator iter(*vector);
+
+        // Fill in the forward-strand start positions and range end_offsets for
+        // each base's answer. TODO: do we want at_least set or not? What does
+        // it do?
+
+        // First cache the forward_start rank we re-use
+        CSA::usint forward_start_rank = iter.rank(range.forward_start, true);
+
+        answers[base].forward_start = start + forward_start_rank;
+        answers[base].end_offset =
+            iter.rank(range.forward_start + range.end_offset, false) -
+            forward_start_rank;
+      }
+    }
+
+    // Since we don't keep an FMDPosition for the non-base end-of-text
+    // character, we need to track its length separately in order for the DP
+    // algorithm given in the paper to be implementable. We calculate
+    // occurrences of the text end character (i.e. how much of the current range
+    // is devoted to things where an endOfText comes next) implicitly: it's
+    // whatever part of the length of the range is unaccounted-for by the other
+    // characters. We need to use the length accessor because ranges with one
+    // thing have the .end_offset set to 0.
+    CSA::usint endOfTextLength = range.getLength();
+
+    for (CSA::usint base = 0; base < NUM_BASES; base++) {
+      // Go through the bases in order and account for their lengths.
+      endOfTextLength -= answers[base].getLength();
+    }
+
+    // The endOfText character is the very first character we need to account
+    // for when subdividing the reverse range and picking which subdivision to
+    // take.
+
+    // Next, allocate the range for the base that comes first in alphabetical
+    // order by reverse complement.
+    answers[0].reverse_start = range.reverse_start + endOfTextLength;
+
+    for (CSA::usint base = 1; base < NUM_BASES; base++) {
+      // For each subsequent base in alphabetical order by reverse complement
+      // (as stored in BASES), allocate it the next part of the reverse range.
+
+      answers[base].reverse_start =
+          answers[base - 1].reverse_start + answers[base - 1].getLength();
+    }
+
+    // Now all the per-base answers are filled in.
+
+    for (CSA::usint base = 0; base < 5; ++base) {
+      // For each base in arbitrary order
+      if (base == c) {
+        // This is the base we're actually supposed to be extending with. Return
+        // its answer.
+        return answers[base];
+      }
+    }
+
+    // If we get here, they gave us something not in BASES somehow.
+    throw "Unrecognized base";
+  } else {
+    // Flip the interval, do backwards search with the reverse complement of the
+    // base, and then flip back.
+    return this->extend(range.flip(), fm6_comp(c), true).flip();
+  }
+}
+
+FMDPosition FMD::fmdCount(const std::string &pattern, bool backward) const {
+  if (pattern.length() == 0) {
+    return this->getSAPosition();
+  }
+
+  // Keep an FMDPosition to store our intermediate result in.
+  FMDPosition index_position;
+
+  if (backward) {
+    // Start at the end of the pattern and work towards the front
+
+    std::string::const_reverse_iterator iter = pattern.rbegin();
+    index_position = this->getCharPosition((CSA::uchar)*iter);
+    if (index_position.isEmpty()) {
+      return index_position;
+    }
+
+    for (++iter; iter != pattern.rend(); ++iter) {
+      // Backwards extend with subsequent characters.
+      index_position = this->extend(index_position, *iter, true);
+      if (index_position.isEmpty()) {
+        return EMPTY_FMD_POSITION;
+      }
+    }
+  } else {
+    // Start at the front of the pattern and work towards the end.
+
+    std::string::const_iterator iter = pattern.begin();
+    index_position = this->getCharPosition((CSA::uchar)*iter);
+    if (index_position.isEmpty()) {
+      return index_position;
+    }
+
+    for (++iter; iter != pattern.end(); ++iter) {
+      // Forwards extend with subsequent characters.
+      index_position = this->extend(index_position, *iter, false);
+      if (index_position.isEmpty()) {
+        return EMPTY_FMD_POSITION;
+      }
+    }
+  }
+
+  this->convertToSAPosition(index_position);
+
+  return index_position;
+}
+
+CSA::usint FMD::extends = 0;
+CSA::usint FMD::restarts = 0;
+
+FMDPosition FMD::getCharPosition(CSA::usint c) const {
+  if (c >= CSA::CHARS || this->array[c] == 0) {
+    return EMPTY_FMD_POSITION;
+  }
+
+  if (!isBase(c)) {
+    return EMPTY_FMD_POSITION;
+  }
+
+  pair_type forward_range = this->alphabet->getRange(c);
+  this->convertToBWTRange(forward_range);
+
+  pair_type reverse_range = this->alphabet->getRange(fm6_comp(c));
+  this->convertToBWTRange(reverse_range);
+
+  // Make the FMDPosition rolling together both ranges.
+  // TODO: Make sure both ranges are the same length, as they should be.
+  FMDPosition position(forward_range.first, reverse_range.first,
+                       forward_range.second - forward_range.first);
+
+  return position;
+}
+
+FMDPosition FMD::getSAPosition() const {
+  return FMDPosition(0, 0, this->data_size - 1);
+}
+
+void FMD::convertToSAPosition(FMDPosition &bwt_position) const {
+  bwt_position.forward_start -= this->number_of_sequences;
+  bwt_position.reverse_start -= this->number_of_sequences;
+}
---
+++ fmd_simple.hpp
@@ -0,0 +1,146 @@
+#ifndef PANPP_FMD_H
+#define PANPP_FMD_H
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "bits/deltavector.h"
+#include "bits/nibblevector.h"
+#include "bits/rlevector.h"
+#include "bits/succinctvector.h"
+#include "rlcsa.h"
+
+#include "utils.hpp"
+
+#ifdef USE_NIBBLE_VECTORS
+// Use Nibble Vectors to encode our range endpoint bitmaps
+typedef CSA::NibbleVector RangeVector;
+typedef CSA::NibbleEncoder RangeEncoder;
+#else
+// Use RLEVectors to encode our range endpoint bitmaps
+typedef CSA::RLEVector RangeVector;
+typedef CSA::RLEEncoder RangeEncoder;
+#endif
+
+typedef std::pair<CSA::usint, CSA::usint> pair_type;
+
+static const CSA::usint NUM_BASES = 5;
+
+extern const std::string BASES; // for debugging purposes
+
+/**
+ * Represents the state (or result) of an FMD-index search, which is two ranges
+ * (one for the forward sequence, and one for the reverse complement) of equal
+ * length. The ranges are stored as two start indices and a length. They can be
+ * in either SA space (not counting the text start symbols at the beginning of
+ * the BWT) or in BWT space.
+ *
+ * Range semantics are inclusive, so a length = 0 range holds 1 thing and its
+ * reverse complement.
+ */
+struct FMDPosition {
+  CSA::usint forward_start;
+  CSA::usint reverse_start;
+  // Offset 0 = only the entry at start/end. -1 = empty.
+  CSA::sint end_offset;
+  FMDPosition();
+  FMDPosition(CSA::usint forward_start, CSA::usint reverse_start,
+              CSA::usint end_offset);
+  /**
+   * Flip the FMDPosition around so the reverse complement interval is the
+   * forward interval and visa versa.
+   */
+  FMDPosition flip() const;
+
+  /**
+   * Are two FMDPositions equal?
+   */
+  bool operator==(const FMDPosition &other) const;
+
+  /**
+   * Is an FMDPosition empty?
+   */
+  inline bool isEmpty() const { return end_offset < 0; }
+
+  /**
+   * Return the actual number of matches represented by an FMDPosition.
+   */
+  inline CSA::usint getLength() const { return end_offset + 1; }
+};
+
+/**
+ * Provide pretty-printing for FMDPositions. See
+ * <http://www.parashift.com/c++-faq/output-operator.html>
+ */
+std::ostream &operator<<(std::ostream &o, FMDPosition const &position);
+
+const FMDPosition EMPTY_FMD_POSITION = FMDPosition(0, 0, -1);
+
+/**
+ * Defines an RLCSA index derivative that represents an FMD-index: an index of
+ * DNA sequences (over the alphabet {A, C, G, T, N}) where all texts are present
+ * with their reverse complements.
+ *
+ * In such an index, an ongoing search can be extended or retracted at either
+ * end in O(1) time.
+ *
+ * See the paper "Exploring single-sample SNP and INDEL calling with whole-
+ * genome de novo assembly" (2012), by Heng Li, which defines the FMD-index.
+ */
+class FMD : public CSA::RLCSA {
+
+public:
+  // We can only be constructed on a previously generated RLCSA index that
+  // just happens to meet our requirements.
+  explicit FMD(const std::string &base_name, bool print = false);
+
+  /**
+   * Extend a search by a character, either backward or forward. Ranges are in
+   * BWT coordinates.
+   */
+  FMDPosition extend(FMDPosition range, CSA::usint c, bool backward) const;
+
+  /**
+   * Count occurrences of a pattern using the FMD search algorithm, iterating
+   * through the pattern either forward or backward.
+   */
+  FMDPosition fmdCount(const std::string &pattern, bool backward = true) const;
+
+  /**
+   * Get an FMDPosition for the part of the BWT for things starting with the
+   * given character.
+   */
+  FMDPosition getCharPosition(CSA::usint c) const;
+
+protected:
+  // How many times did we extend a search while mapping?
+  static CSA::usint extends;
+
+  // How many times did we restart a search while mapping?
+  static CSA::usint restarts;
+
+private:
+  /**
+   * Get an FMDPosition covering the whole SA.
+   */
+  FMDPosition getSAPosition() const;
+
+  /**
+   * Convert an FMDPosition in BWT coordinates to one in SA coordinates, in
+   * place.
+   */
+  void convertToSAPosition(FMDPosition &bwt_position) const;
+
+  // These are not allowed.
+  FMD();
+  FMD(const FMD &);
+  FMD &operator=(const FMD &);
+};
+
+#endif
