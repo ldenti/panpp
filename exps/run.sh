b9475250906a4672ffb5d09b54ea7bb269c12e63
Luca Denti <ldenti@proton.me>
2023-11-16 10:17:37 +0100

    Add evaluation on specifics from assembly

--- exps/run.sh
+++ exps/run.sh
@@ -37,7 +37,8 @@ then
     WD=$3
     log "Extracting haplotypes"
     mkdir -p $WD/seqs
-    agc listset $AGC | (RANDOM=$SEED; while read line ; do echo "$RANDOM $line" ; done ) | sort | head -n ${Ns[${#Ns[@]}-1]} | cut -f2 -d' ' > $WD/hlist.txt
+    np1=$((${Ns[${#Ns[@]}-1]} + 1))
+    agc listset $AGC | (RANDOM=$SEED; while read line ; do echo "$RANDOM $line" ; done ) | sort | head -n $np1 | cut -f2 -d' ' > $WD/hlist.txt
 
     # Get sequences of selected haplotypes
     while read line
@@ -55,6 +56,9 @@ then
 	done
 	cat $WD/$n/seqs/*.fa > $WD/$n/seqs.fa
     done
+
+    idx=$(tail -1 $WD/hlist.txt)
+    ln -sf $WD/seqs/$idx.fa $WD/$idx.fa
 elif [[ $mode == "index1" ]]
 then
     WD=$2
@@ -74,12 +78,17 @@ then
     WD=$2
     for n in "${Ns[@]}"
     do
+	if [ "$n" -gt "4" ]
+	then
+	    log "NOT indexing $n haplotypes"
+	    continue
+	fi
 	if [ ! -f $WD/$n/index.fmd ]
 	then
 	    log "Indexing $n haplotypes"
 	    \time -vo $WD/$n/SVDSS-index.time SVDSS index --fasta $WD/$n/seqs.fa --index $WD/$n/index.fmd
 	else
-	    log "NOT indexing $n haplotypes"
+	    log "Skipping indexing $n haplotypes"
 	fi
     done
 elif [[ $mode == "search1" ]]
@@ -96,9 +105,25 @@ then
 		log "Searching from ${cov}x sample against $n haplotypes"
 		\time -vo $WD/$n/${cov}x/PANPP-search.time ../PANPP search $WD/$n/index $WD/reads/${cov}x.fq -@ $threads > $WD/$n/${cov}x/PANPP.sfs
 	    else
-	        log "NOT searching from ${cov}x sample against $n haplotypes"
+	        log "Skipping searching from ${cov}x sample against $n haplotypes"
 	    fi
 	done
+    done
+elif [[ $mode == "search11" ]]
+then
+    WD=$2
+    threads=$3
+    idx=$(basename $WD/*.fa .fa)
+    for n in "${Ns[@]}"
+    do
+	if [ ! -f $WD/$n/$idx/PANPP.sfs ]
+	then
+	    mkdir -p $WD/$n/$idx/
+	    log "Searching from $idx against $n haplotypes"
+	    \time -vo $WD/$n/$idx/PANPP-search.time ../PANPP search $WD/$n/index $WD/$idx.fa -@ $threads > $WD/$n/$idx/PANPP.sfs
+	else
+	    log "Skipping searching from $idx against $n haplotypes"
+	fi
     done
 elif [[ $mode == "search2" ]]
 then
@@ -106,6 +131,11 @@ then
     threads=$3
     for n in "${Ns[@]}"
     do
+	if [ "$n" -gt "4" ]
+	then
+	    log "NOT searching from ${cov}x sample against $n haplotypes"
+	    continue
+	fi
 	for cov in "${COV[@]}"
 	do
 	    if [ ! -f $WD/$n/${cov}x/SVDSS.sfs ]
@@ -115,9 +145,26 @@ then
 		\time -vo $WD/$n/${cov}x/SVDSS-search.time SVDSS search --index $WD/$n/index.fmd --fastq $WD/reads/${cov}x.fq --workdir $WD/$n/${cov}x/SVDSS --threads $threads
 		cat $WD/$n/${cov}x/SVDSS/*.sfs > $WD/$n/${cov}x/SVDSS.sfs
 	    else
-	        log "NOT searching from ${cov}x sample against $n haplotypes"
+	        log "Skipping searching from ${cov}x sample against $n haplotypes"
 	    fi
 	done
+    done
+elif [[ $mode == "search22" ]]
+then
+    WD=$2
+    threads=$3
+    idx=$(basename $WD/*.fa .fa)
+    for n in "${Ns[@]}"
+    do
+	if [ ! -f $WD/$n/$idx/SVDSS.sfs ]
+	then
+	    mkdir -p $WD/$n/$idx/
+	    log "Searching from $idx against $n haplotypes"
+	    \time -vo $WD/$n/$idx/SVDSS-search.time SVDSS search --index $WD/$n/index.fmd --fastq $WD/$idx.fa --workdir $WD/$n/$idx/SVDSS --threads $threads
+	    cat $WD/$n/$idx/SVDSS/*.sfs > $WD/$n/$idx/SVDSS.sfs
+	else
+	    log "Skipping searching from $idx against $n haplotypes"
+	fi
     done
 elif [[ $mode == "compare" ]]
 then
